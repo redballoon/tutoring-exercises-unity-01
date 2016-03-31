5465d1992edfa040896385098e9a3807615fc502
redballoon <fred@Edmeisters-MacBook-Air.local>
2016-03-31 02:00:10 -0400

    initial project files

---
+++ Roll a Ball/Assets/Scripts/PlayerController.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var rb:Rigidbody;
+
+function Start () {
+	rb = GetComponent('Rigidbody');
+}
+
+function Update () {
+
+}
+
+function FixedUpdate() {
+	var moveX:float = Input.GetAxis('Horizontal');
+	var moveY:float = Input.GetAxis('Vertical');
+
+	var force:Vector3 = new Vector3(moveX, 0, moveY);
+
+	rb.AddForce(force);
+}
